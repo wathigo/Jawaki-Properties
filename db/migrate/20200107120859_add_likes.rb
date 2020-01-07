b1d555a28b496c2330a6bd72e46bb7bcfb8e9460
wathigo <wathigosimon@gmail.com>
2020-01-07 07:12:05 -0500

    Fix a bug in likes migration

--- db/migrate/20200107120859_add_likes.rb
+++ db/migrate/20200107120859_add_likes.rb
@@ -1,12 +1,10 @@
-class Likes < ActiveRecord::Migration[5.2]
+class AddLikes < ActiveRecord::Migration[5.2]
   def change
-    def change
-      create_table :likes do |t|
+    create_table :likes do |t|
         t.references :user, foreign_key: true
         t.references :property, foreign_key: true
 
         t.timestamps
       end
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_05_160626) do
+ActiveRecord::Schema.define(version: 2020_01_07_120859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_01_05_160626) do
     t.string "prop_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "property_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_likes_on_property_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "properties", force: :cascade do |t|
@@ -48,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_01_05_160626) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "properties"
+  add_foreign_key "likes", "users"
   add_foreign_key "properties", "categories"
 end
