b7e729562592312a2cd2ba0f92e8afdfd63aad05
wathigo <wathigosimon@gmail.com>
2020-01-09 10:54:09 -0500

    Update property:show page && update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ADDRESSES = [
-  '311 RT 9W, Glenmont NY 12077',
-  '425 Route 31, Macedon NY 14502',
-  '352 Palmer Road, Ware MA 108',
-  '700 Oak Street, Brockton MA 2301',
-  '288 Larkin, Monroe NY 10950',
-  '591 Memorial Dr, Chicopee MA 1020',
-  '255 W Main St, Avon CT 6001',
-  '67 Newton Rd, Danbury CT 6810',
-  '337 Russell St, Hadley MA 103',
-  '3949 Route 31, Clay NY 13041',
-  '250 Rt 59, Airmont NY 10901'
-]
-
-def random_point(max_radius=57)
-  r = max_radius * rand ** 0.5
-  theta = rand * 2 * Math::PI
-  [r * Math.cos(theta), r * Math.sin(theta)]
-end
-
 categories = ['Housing Properties', 'Land Properties']
 categories.each do |n|
   Category.create!(prop_type: n)
@@ -36,17 +16,14 @@ categories = Category.all
 all_status = ['booked', 'reserved', 'bought']
 
 categories.each do |category|
-  points = random_point
-  random_latt = points[0]
-  random_long = points[1]
   8.times do |n|
     category.properties.create!(
       name: Faker::Name.unique.name,
-      address: ADDRESSES.sample,
-      description: Faker::Games::WorldOfWarcraft.quote,
+      address: Faker::Address.full_address,
+      description: Faker::Lorem.paragraph(sentence_count: 30),
       price: (200000 + rand(1000000)),
-      latt: random_latt,
-      long: random_long,
+      latt: Faker::Address.latitude,
+      long: Faker::Address.longitude,
       status: all_status.sample
     )
   end
