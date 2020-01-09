dfcb50e5eafbc2bb430da476d34a2fa8e2755dca
wathigo <wathigosimon@gmail.com>
2020-01-09 17:29:31 -0500

    Update zoom value

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -2,7 +2,7 @@ const initMap = (lat, lng) => {
   const myCoords = new google.maps.LatLng(lat, lng);
   const mapOptions = {
   center: myCoords,
-  zoom: 14
+  zoom: 5
   };
   const map = new google.maps.Map(document.getElementById('map'), mapOptions);
   const marker = new google.maps.Marker({
