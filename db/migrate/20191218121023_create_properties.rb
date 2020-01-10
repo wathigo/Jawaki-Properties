ee5f4b33de19443302b7a11168761a31cbd3b462
wathigo <wathigosimon@gmail.com>
2020-01-10 03:37:16 -0500

    Update migration

--- db/migrate/20191218121023_create_properties.rb
+++ db/migrate/20191218121023_create_properties.rb
@@ -4,7 +4,7 @@ class CreateProperties < ActiveRecord::Migration[5.2]
       t.references :category, foreign_key: true
       t.string :name
       t.string :address
-      t.string :status
+      t.string :status, :default => 'available'
       t.string :description
       t.integer :price
       t.float :latt
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_120859) do
     t.bigint "category_id"
     t.string "name"
     t.string "address"
-    t.string "status"
+    t.string "status", default: "available"
     t.string "description"
     t.integer "price"
     t.float "latt"
